17af4bebfc7a685d7fafba72c9c989038a1297fc
mayank <mayankgandhi50@gmail.com>
2025-11-28 15:35:19 +0530

    moved anycodable

--- Sources/LLMKit/OpenAI/OpenAIModels.swift
+++ Sources/LLMKit/OpenAI/OpenAIModels.swift
@@ -380,58 +380,6 @@ public struct OpenAIJSONSchema: Codable {
 
 }
 
-public struct AnyCodable: Codable {
-    public let value: Any
-    
-    public init<T>(_ value: T?) {
-        self.value = value ?? ()
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-
-        if container.decodeNil() {
-            value = ()
-        } else if let bool = try? container.decode(Bool.self) {
-            value = bool
-        } else if let int = try? container.decode(Int.self) {
-            value = int
-        } else if let double = try? container.decode(Double.self) {
-            value = double
-        } else if let string = try? container.decode(String.self) {
-            value = string
-        } else if let array = try? container.decode([AnyCodable].self) {
-            value = array.map { $0.value }
-        } else if let dictionary = try? container.decode([String: AnyCodable].self) {
-            value = dictionary.mapValues { $0.value }
-        } else {
-            value = ()
-        }
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-
-        switch value {
-        case is Void:
-            try container.encodeNil()
-        case let int as Int:
-            try container.encode(int)
-        case let double as Double:
-            try container.encode(double)
-        case let bool as Bool:
-            try container.encode(bool)
-        case let string as String:
-            try container.encode(string)
-        case let array as [Any]:
-            try container.encode(array.map { AnyCodable($0) })
-        case let dictionary as [String: Any]:
-            try container.encode(dictionary.mapValues { AnyCodable($0) })
-        default:
-            try container.encodeNil()
-        }
-    }
-}
 
 public struct OpenAIChatResponse: Codable {
     public let id: String
